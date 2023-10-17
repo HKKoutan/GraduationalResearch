e0dd874bc5e79dacf37d2b279d687e7cd809cce1
HK_Koutan <kouseisinn@gmail.com>
2023-10-17 13:44:00 +0900

    refactor: RandomBitsの構築に長さを要求しないように変更

--- src/common/randombits.hpp
+++ src/common/randombits.hpp
@@ -8,17 +8,17 @@
 
 namespace util {
 
-template<std::size_t L>
 class RandomBits {
 	std::mt19937_64 mt;
 public:
 	explicit RandomBits(std::int64_t seed): mt(seed){}
 	RandomBits() noexcept: RandomBits(0i64){}
+	template<std::size_t L>
 	void generate(std::bitset<L> &bits);
 };
 
 template<std::size_t L>
-void RandomBits<L>::generate(std::bitset<L> &bits){
+void RandomBits::generate(std::bitset<L> &bits){
 	constexpr auto L64 = L/64u;
 	for(std::size_t i=0u, iend=L64; i<iend; ++i){
 		std::bitset<64u> rand(mt());
