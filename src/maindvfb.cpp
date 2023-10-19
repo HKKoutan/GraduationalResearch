31e47a8f6d27978e66585a22deb223cee2a99ffe
HK_Koutan <kouseisinn@gmail.com>
2023-10-19 19:42:22 +0900

    fix: nterrorの集計ができていなかったのを修正

--- src/maindvfb.cpp
+++ src/maindvfb.cpp
@@ -38,12 +38,12 @@ int main(int argc, char* argv[]){
 
 	code::Systematic_LDPC<SOURCE_LENGTH,CODE_LENGTH> ldpc;
 	// tuple: biterrors, bitcounts, nterrors, ntcounts, GCper(average,var)
-	array<tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,pair<double,double>>,2> stat = {};
-	array<tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<vector<double>,nsize>>,NUM_THREADS> stats = {};
+	array<tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,pair<double,double>>,2> stat = {};
+	array<tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<vector<double>,nsize>>,NUM_THREADS> stats = {};
 
-	auto plain = [&ldpc, repeat_per_thread](size_t t, tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<vector<double>,nsize>> *st){
-		auto &biterror = std::get<0>(*st), &bitcount = std::get<1>(*st), &nterror = std::get<2>(*st), &ntcount = std::get<3>(*st);
-		auto &gcper = std::get<4>(*st);
+	auto plain = [repeat_per_thread](size_t t, tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<vector<double>,nsize>> *st){
+		auto &biterror = std::get<0>(*st), &bitcount = std::get<1>(*st), &nterror = std::get<2>(*st);
+		auto &gcper = std::get<3>(*st);
 		for(size_t n=0; n<nsize; ++n){
 			bitset<SOURCE_LENGTH> m;
 			channel::Nanopore_Sequencing ch(noise_factor[n],t);
@@ -65,18 +65,17 @@ int main(int argc, char* argv[]){
 				{
 					uint64_t acc = 0u;
 					for(size_t i=0u, iend=cm.size(); i<iend; ++i) acc += (cm[i]!=rm[i]);
-					nterror[n] = acc;
+					nterror[n] += acc;
 				}
-				ntcount[n] += cm.size();
 				biterror[n] += (mest^(m&mmask)).count();
 				bitcount[n] += mmask.count();
 			}
 		}
 	};
 
-	auto encoded = [&ldpc, repeat_per_thread](size_t t, tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<vector<double>,nsize>> *st){
-		auto &biterror = std::get<0>(*st), &bitcount = std::get<1>(*st), &nterror = std::get<2>(*st), &ntcount = std::get<3>(*st);
-		auto &gcper = std::get<4>(*st);
+	auto encoded = [&ldpc, repeat_per_thread](size_t t, tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,array<vector<double>,nsize>> *st){
+		auto &biterror = std::get<0>(*st), &bitcount = std::get<1>(*st), &nterror = std::get<2>(*st);
+		auto &gcper = std::get<3>(*st);
 		auto decoder = ldpc.make_decoder<code::LDPC::SumProduct_Decoding<SOURCE_LENGTH,CODE_LENGTH>>();
 		for(size_t n=0; n<nsize; ++n){
 			bitset<SOURCE_LENGTH> m;
@@ -109,9 +108,8 @@ int main(int argc, char* argv[]){
 				{
 					uint64_t acc = 0u;
 					for(size_t i=0u, iend=cm.size(); i<iend; ++i) acc += (cm[i]!=cest[i]);
-					nterror[n] = acc;
+					nterror[n] += acc;
 				}
-				ntcount[n] += cm.size();
 				biterror[n] += (mest^(m&mmask)).count();
 				bitcount[n] += mmask.count();
 			}
@@ -127,13 +125,12 @@ int main(int argc, char* argv[]){
 			std::get<0>(stat[0])[n] += std::get<0>(st)[n];
 			std::get<1>(stat[0])[n] += std::get<1>(st)[n];
 			std::get<2>(stat[0])[n] += std::get<2>(st)[n];
-			std::get<3>(stat[0])[n] += std::get<3>(st)[n];
 		}
 	}
 	{
 		auto sum = 0.0, sqsum = 0.0;
 		for(auto &st: stats){
-			for(auto &pn: std::get<4>(st)){
+			for(auto &pn: std::get<3>(st)){
 				for(auto &pr: pn){
 					sum += pr;
 					sqsum += pr*pr;
@@ -142,8 +139,8 @@ int main(int argc, char* argv[]){
 		}
 		auto num = static_cast<double>(nsize*NUM_THREADS*repeat_per_thread);
 		auto average = sum/num;
-		std::get<4>(stat[0]).first = average;
-		std::get<4>(stat[0]).second = sqsum/num - average*average;
+		std::get<3>(stat[0]).first = average;
+		std::get<3>(stat[0]).second = sqsum/num - average*average;
 	}
 	threads.clear();
 	tk.split();
@@ -154,13 +151,12 @@ int main(int argc, char* argv[]){
 			std::get<0>(stat[1])[n] += std::get<0>(st)[n];
 			std::get<1>(stat[1])[n] += std::get<1>(st)[n];
 			std::get<2>(stat[1])[n] += std::get<2>(st)[n];
-			std::get<3>(stat[1])[n] += std::get<3>(st)[n];
 		}
 	}
 	{
 		auto sum = 0.0, sqsum = 0.0;
 		for(auto &st: stats){
-			for(auto &pn: std::get<4>(st)){
+			for(auto &pn: std::get<3>(st)){
 				for(auto &pr: pn){
 					sum += pr;
 					sqsum += pr*pr;
@@ -169,14 +165,14 @@ int main(int argc, char* argv[]){
 		}
 		auto num = static_cast<double>(nsize*NUM_THREADS*repeat_per_thread);
 		auto average = sum/num;
-		std::get<4>(stat[1]).first = average;
-		std::get<4>(stat[1]).second = sqsum/num - average*average;
+		std::get<3>(stat[1]).first = average;
+		std::get<3>(stat[1]).second = sqsum/num - average*average;
 	}
 	tk.stop();
 
 	cout<<SOURCE_LENGTH<<endl;
 	cout<<"plain"<<endl;
-	cout<<"GCper var: "<<std::get<4>(stat[0]).second<<", ave: "<<std::get<4>(stat[0]).first<<endl;
+	cout<<"GCper var: "<<std::get<3>(stat[0]).second<<", ave: "<<std::get<3>(stat[0]).first<<endl;
 	cout<<"Noise factor"
 	<<"\tBER"
 	<<"\tNER"
@@ -185,12 +181,12 @@ int main(int argc, char* argv[]){
 	for(size_t n=0; n<noise_factor.size(); n++){
 		cout<<noise_factor[n]
 		<<"\t"<<static_cast<double>(std::get<0>(stat[0])[n])/static_cast<double>(std::get<1>(stat[0])[n])
-		<<"\t"<<static_cast<double>(std::get<2>(stat[0])[n])/static_cast<double>(std::get<3>(stat[0])[n])
+		<<"\t"<<static_cast<double>(std::get<2>(stat[0])[n])/static_cast<double>(SOURCE_LENGTH/2*NUM_THREADS*repeat_per_thread)
 		<<endl;
 	}
 	cout<<SOURCE_LENGTH<<"->"<<CODE_LENGTH<<endl;
 	cout<<"encoded(no balancing)"<<endl;
-	cout<<"GCper var: "<<std::get<4>(stat[1]).second<<", ave: "<<std::get<4>(stat[1]).first<<endl;
+	cout<<"GCper var: "<<std::get<3>(stat[1]).second<<", ave: "<<std::get<3>(stat[1]).first<<endl;
 	cout<<"Noise factor"
 	<<"\tBER"
 	<<"\tNER"
@@ -199,7 +195,7 @@ int main(int argc, char* argv[]){
 	for(size_t n=0; n<noise_factor.size(); n++){
 		cout<<noise_factor[n]
 		<<"\t"<<static_cast<double>(std::get<0>(stat[1])[n])/static_cast<double>(std::get<1>(stat[1])[n])
-		<<"\t"<<static_cast<double>(std::get<2>(stat[1])[n])/static_cast<double>(std::get<3>(stat[1])[n])
+		<<"\t"<<static_cast<double>(std::get<2>(stat[1])[n])/static_cast<double>(CODE_LENGTH/2*NUM_THREADS*repeat_per_thread)
 		<<endl;
 	}
 	return 0;
