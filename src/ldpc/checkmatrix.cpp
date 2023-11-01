1ec37e976addff14aa9dcef8cb7169d6eb1f1bca
HK_Koutan <kouseisinn@gmail.com>
2023-11-01 23:35:11 +0900

    refactor: 定義をプリプロセッサマクロで置き換え

--- src/ldpc/checkmatrix.cpp
+++ src/ldpc/checkmatrix.cpp
@@ -1,21 +1,18 @@
 ﻿#include "checkmatrix.hpp"
 
-using std::size_t;
+#define SET_CheckMatrix_regular(P,S,C,W) const char *CheckMatrix_regular<S,C,W>::path = P; template<> auto getCheckMatrix<S,C>(){return CheckMatrix_regular<S,C,W>();}
+#define SET_CheckMatrix_irregular(P,S,C) const char *CheckMatrix_irregular<S,C>::path = P; template<> auto getCheckMatrix<S,C>(){return CheckMatrix_irregular<S,C>();}
 
 namespace code::LDPC {
 
-const char *CheckMatrix_regular<252,504,6>::path = "H.txt";
-const char *CheckMatrix_regular<256,512,6>::path = "36_512.txt";
-const char *CheckMatrix_regular<512,1024,6>::path = "36_1024.txt";
-const char *CheckMatrix_regular<1024,2048,6>::path = "36_2048.txt";
-const char *CheckMatrix_regular<5000,10000,6>::path = "H2.txt";
-const char *CheckMatrix_irregular<4999,10000>::path = "H3.txt";
-
-template<> auto getCheckMatrix<252,504>(){return CheckMatrix_regular<252,504,6>();}
-template<> auto getCheckMatrix<256,512>(){return CheckMatrix_regular<256,512,6>();}
-template<> auto getCheckMatrix<512,1024>(){return CheckMatrix_regular<512,1024,6>();}
-template<> auto getCheckMatrix<1024,2048>(){return CheckMatrix_regular<1024,2048,6>();}
-template<> auto getCheckMatrix<5000,10000>(){return CheckMatrix_regular<5000,10000,6>();}
-template<> auto getCheckMatrix<4999,10000>(){return CheckMatrix_irregular<4999,10000>();}
+SET_CheckMatrix_regular("H.txt",252,504,6)
+SET_CheckMatrix_regular("36_512.txt",256,512,6)
+SET_CheckMatrix_regular("36_1024.txt",512,1024,6)
+SET_CheckMatrix_regular("36_2048.txt",1024,2048,6)
+SET_CheckMatrix_regular("H2.txt",5000,10000,6)
+SET_CheckMatrix_irregular("H3.txt",4999,10000)
 
 }
+
+#undef SET_CheckMatrix_regular
+#undef SET_CheckMatrix_irregular
