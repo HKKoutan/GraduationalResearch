fa2fb4f25262e4d64191576b8db26aa7286c5ded
HK_Koutan <kouseisinn@gmail.com>
2023-11-03 19:28:24 +0900

    test: 3.11.1_awgn_b2 irregular performance test

--- src/mainawgn.cpp
+++ src/mainawgn.cpp
@@ -9,8 +9,8 @@ using std::size_t, std::uint64_t;
 using std::cout, std::cerr, std::flush, std::endl;
 
 constexpr size_t DEFAULT_REPEAT_PER_THREAD = 1000;
-constexpr size_t SOURCE_LENGTH = 252;
-constexpr size_t CODE_LENGTH = 504;
+constexpr size_t SOURCE_LENGTH = 5001;
+constexpr size_t CODE_LENGTH = 10000;
 constexpr size_t NUM_THREADS = 12;
 
 int main(int argc, char* argv[]){
