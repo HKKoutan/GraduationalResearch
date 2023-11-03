acd980caae5acf3cf6283f66a048c5ea48615f3f
HK_Koutan <kouseisinn@gmail.com>
2023-11-03 19:04:04 +0900

    fix: H3の情報長を修正

--- src/ldpc/LDPCCheckMatrix.hpp
+++ src/ldpc/LDPCCheckMatrix.hpp
@@ -71,7 +71,7 @@ SET_CheckMatrix_regular("36_512.txt",256,512,6)
 SET_CheckMatrix_regular("36_1024.txt",512,1024,6)
 SET_CheckMatrix_regular("36_2048.txt",1024,2048,6)
 SET_CheckMatrix_regular("H2.txt",5000,10000,6)
-SET_CheckMatrix_irregular("H3.txt",4999,10000)
+SET_CheckMatrix_irregular("H3.txt",5001,10000)
 
 #undef SET_CheckMatrix_regular
 #undef SET_CheckMatrix_irregular
