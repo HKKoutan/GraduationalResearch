4b098d207ad8e6d63d7ad25421ba7888f0f21887
HK_Koutan <kouseisinn@gmail.com>
2023-12-10 12:24:54 +0900

    feature: convertの特殊化を追加

--- src/dnas/codeDNASadapter.hpp
+++ src/dnas/codeDNASadapter.hpp
@@ -35,8 +35,24 @@ public:
 	static auto decode_p(const std::array<nucleotide_p<ATGC,T>,S> &code, nucleotide_p<ATGC,T> initial_state = {1,0,0,0});
 };
 
-template<std::uint8_t ATGC>
-struct convert {
+template<std::uint8_t ATGC> struct convert;
+
+template<>
+class convert<0x1B> {
+	static constexpr std::uint8_t ATGC = 0x1B;
+public:
+	template<std::size_t S>
+	static auto nttype_to_binary(const std::array<nucleotide_t<ATGC>,S> &source);
+	template<std::floating_point T, std::size_t S>
+	static auto nttype_to_binary_p(const std::array<nucleotide_p<ATGC,T>,S> &source);
+	template<std::size_t S>
+	static auto binary_to_nttype(const std::bitset<S> &source);
+};
+
+template<>
+class convert<0x27> {
+	static constexpr std::uint8_t ATGC = 0x27;
+public:
 	template<std::size_t S>
 	static auto nttype_to_binary(const std::array<nucleotide_t<ATGC>,S> &source);
 	template<std::floating_point T, std::size_t S>
@@ -163,20 +179,18 @@ auto differential<0x27>::decode_p(const std::array<nucleotide_p<ATGC,T>,S> &code
 //                                                            //
 ////////////////////////////////////////////////////////////////
 
-template<std::uint8_t ATGC>
 template<std::size_t S>
-auto convert<ATGC>::nttype_to_binary(const std::array<nucleotide_t<ATGC>,S> &source){
-	std::bitset<S*2> code;
+auto convert<0x1B>::nttype_to_binary(const std::array<nucleotide_t<ATGC>,S> &source){
+	std::bitset<S*2> result;
 	for(std::size_t i=0; const auto &j: source){
-		code[i++]=j.msb();
-		code[i++]=j.lsb();
+		result[i++]=j.msb();
+		result[i++]=j.lsb();
 	}
-	return code;
+	return result;
 }
 
-template<std::uint8_t ATGC>
 template<std::floating_point T, std::size_t S>
-auto convert<ATGC>::nttype_to_binary_p(const std::array<nucleotide_p<ATGC,T>,S> &source){
+auto convert<0x1B>::nttype_to_binary_p(const std::array<nucleotide_p<ATGC,T>,S> &source){
 	std::array<T,S*2> LLR;
 	for(std::size_t i=0; const auto &j: source){
 		auto PX0 = j[0]+j[2], PX1 = j[1]+j[3], P0X = j[0]+j[1], P1X = j[2]+j[3];
@@ -192,16 +206,54 @@ auto convert<ATGC>::nttype_to_binary_p(const std::array<nucleotide_p<ATGC,T>,S> 
 	return LLR;
 }
 
-template<std::uint8_t ATGC>
 template<std::size_t S>
-auto convert<ATGC>::binary_to_nttype(const std::bitset<S> &source){
+auto convert<0x1B>::binary_to_nttype(const std::bitset<S> &source){
 	static_assert(S%2==0);
-	std::array<nucleotide_t<ATGC>,S/2> code;
-	for(std::size_t i=0; auto &j: code){
+	std::array<nucleotide_t<ATGC>,S/2> result;
+	for(std::size_t i=0; auto &j: result){
 		j = static_cast<int>(source.test(i++))<<1;
 		j += static_cast<int>(source.test(i++));
 	}
-	return code;
+	return result;
+}
+
+template<std::size_t S>
+auto convert<0x27>::nttype_to_binary(const std::array<nucleotide_t<ATGC>,S> &source){
+	std::bitset<S*2> result;
+	for(std::size_t i=0; const auto &j: source){
+		auto msb = j.msb();
+		result[i++] = msb;
+		result[i++] = j.lsb()^msb;
+	}
+	return result;
+}
+
+template<std::floating_point T, std::size_t S>
+auto convert<0x27>::nttype_to_binary_p(const std::array<nucleotide_p<ATGC,T>,S> &source){
+	std::array<T,S*2> LLR;
+	for(std::size_t i=0; const auto &j: source){
+		auto PX0 = j[0]+j[3], PX1 = j[1]+j[2], P0X = j[0]+j[1], P1X = j[2]+j[3];
+		if(P0X==0) LLR[i] = -std::numeric_limits<T>::infinity();
+		else if(P1X==0) LLR[i] = std::numeric_limits<T>::infinity();
+		else LLR[i] = std::log(P0X)-std::log(P1X);
+		++i;
+		if(PX0==0) LLR[i] = -std::numeric_limits<T>::infinity();
+		else if(PX1==0) LLR[i] = std::numeric_limits<T>::infinity();
+		else LLR[i] = std::log(PX0)-std::log(PX1);
+		++i;
+	}
+	return LLR;
+}
+
+template<std::size_t S>
+auto convert<0x27>::binary_to_nttype(const std::bitset<S> &source){
+	static_assert(S%2==0);
+	std::array<nucleotide_t<ATGC>,S/2> result;
+	for(std::size_t i=0; auto &j: result){
+		auto msb = source.test(i++);
+		j = (static_cast<int>(msb)<<1)+static_cast<int>(source.test(i++)^msb);
+	}
+	return result;
 }
 
 }
