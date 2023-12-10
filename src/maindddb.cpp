87365b638bfcfabc64d85afb42bc661fc982940a
HK_Koutan <kouseisinn@gmail.com>
2023-12-10 22:45:37 +0900

    chore: 定数を復元

--- src/maindddb.cpp
+++ src/maindddb.cpp
@@ -18,7 +18,7 @@ using code::DNAS::nucleotide_t;
 constexpr size_t DEFAULT_REPEAT_PER_THREAD = 1000;
 constexpr size_t SOURCE_LENGTH = 512;
 constexpr size_t CODE_LENGTH = 1024;
-constexpr size_t NUM_THREADS = 1;
+constexpr size_t NUM_THREADS = 12;
 constexpr size_t BLOCK_SIZE = 16;
 constexpr uint8_t ATGC = 0x1B;
 constexpr double TOLERANCE = 0.125;
