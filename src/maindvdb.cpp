5bd9433a4cbb1757866fb2d574f23d08897ee713
HK_Koutan <kouseisinn@gmail.com>
2023-12-13 19:23:20 +0900

    feature: 変更を適用

--- src/maindvdb.cpp
+++ src/maindvdb.cpp
@@ -42,6 +42,7 @@ int main(int argc, char* argv[]){
 	constexpr array noise_factor = {0.04,0.03,0.02,0.01,0.0};
 	// constexpr array noise_factor = {0.04,0.035,0.03,0.025,0.02,0.015,0.01,0.005,0.0};
 	constexpr size_t nsize = noise_factor.size();
+	code::LDPC::phi_table<> decodertype;
 
 	auto ldpc = code::make_SystematicLDPC<SOURCE_LENGTH,CODE_LENGTH>();
 	// tuple: biterrors, bitcounts, nterrors, maxGCdeviation, maxrunlength
@@ -62,7 +63,7 @@ int main(int argc, char* argv[]){
 
 				auto [cm, mmask, run] = code::DNAS::VLRLL<ATGC>::encode(m);
 
-				auto cmbar = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::balance(cm);
+				auto [cmbar, c1] = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::balance(cm);
 				// auto bm = cm;
 
 				auto dev = code::DNAS::countBlockGCmaxDeviation<BLOCK_SIZE>(cmbar);
@@ -74,14 +75,14 @@ int main(int argc, char* argv[]){
 				// auto rm=cm;
 
 				auto mest = code::DNAS::VLRLL<ATGC>::decode(rm);
-				nterror[n] += code::DNAS::countDifferentialError(cm, rm);
+				nterror[n] += code::DNAS::countDifferentialError(cm,rm);
 				biterror[n] += ((mest&mmask)^(m&mmask)).count();
 				bitcount[n] += mmask.count();
 			}
 		}
 	};
 
-	auto encoded = [&ldpc, repeat_per_thread](size_t t, tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,uint64_t,size_t> *st){
+	auto encoded = [&ldpc, &decodertype, repeat_per_thread](size_t t, tuple<array<uint64_t,nsize>,array<uint64_t,nsize>,array<uint64_t,nsize>,uint64_t,size_t> *st){
 		auto &biterror = std::get<0>(*st), &bitcount = std::get<1>(*st), &nterror = std::get<2>(*st);
 		auto &maxgcdev = std::get<3>(*st);
 		auto &maxrunlength = std::get<4>(*st);
@@ -94,12 +95,14 @@ int main(int argc, char* argv[]){
 				rb.generate(m);
 
 				auto [cm, mmask, run] = code::DNAS::VLRLL<ATGC>::encode(m);
-				auto tm = code::DNAS::differential<ATGC>::decode(cm);
+				auto cmd = code::DNAS::differential::decode(cm);
+				auto tm = code::DNAS::convert<ATGC>::nttype_to_binary(cmd);
 				auto tr = ldpc.encode_redundancy(tm);
-				auto cr = code::DNAS::modifiedVLRLL<ATGC>::encode(tr, cm.back(), run);
+				auto crd = code::DNAS::convert<ATGC>::binary_to_nttype(tr);
+				auto cr = code::DNAS::puncturedRLL<ATGC>::encode(crd, cm.back(), run);
 
-				auto cmbar = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::balance(cm);
-				auto crbar = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::balance(cr, cmbar.back()-cm.back());
+				auto [cmbar, c1] = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::balance(cm);
+				auto [crbar, c2] = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::balance(cr, c1);
 				// auto bm = cm;
 				// auto br = cr;
 
@@ -115,17 +118,22 @@ int main(int argc, char* argv[]){
 				// auto rm=bm;
 				// auto rr=br;
 
-				auto Lrm = ch.likelihood<float>(rm);
-				auto Lrr = ch.likelihood<float>(rr);
-				auto LLR = code::concatenate(code::DNAS::differential<ATGC>::decode_p(Lrm), code::DNAS::modifiedVLRLL<ATGC>::decode_p(Lrr, Lrm.back()));
+				auto Lcmbar = ch.likelihood<float>(rm);
+				auto Lcrbar = ch.likelihood<float>(rr);
+				auto Lcmdbar = code::DNAS::differential::decode_p(Lcmbar);
+				auto Lcrdbar = code::DNAS::puncturedRLL<ATGC>::decode_p(Lcrbar, Lcmbar.back());
+				auto [Lcmd, p1] = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::restore_p(Lcmdbar);
+				auto [Lcrd, p2] = code::DNAS::DivisionBalancing<ATGC,BLOCK_SIZE,1>::restore_p(Lcrdbar,p1);
+				auto LLR = code::concatenate(code::DNAS::convert<ATGC>::nttype_to_binary_p(Lcmd), code::DNAS::convert<ATGC>::nttype_to_binary_p(Lcrd));
 
-				auto LLRest = ldpc.decode<decltype(ldpc)::DecoderType::SumProduct>(LLR);
+				auto LLRest = ldpc.decode(LLR,decodertype);
 				// auto LLRest = LLR;
-				auto test = code::estimate_crop<SOURCE_LENGTH>(LLRest);
-				auto cest = code::DNAS::differential<ATGC>::encode(test);
-				auto mest = code::DNAS::VLRLL<ATGC>::decode(cest);
+				auto tmest = code::estimate_crop<SOURCE_LENGTH>(LLRest);
+				auto cmdest = code::DNAS::convert<ATGC>::binary_to_nttype(tmest);
+				auto cmest = code::DNAS::differential::encode(cmdest);
+				auto mest = code::DNAS::VLRLL<ATGC>::decode(cmest);
 
-				nterror[n] += code::DNAS::countDifferentialError(cm, cest);
+				nterror[n] += code::DNAS::countDifferentialError(cm,rm);
 				biterror[n] += ((mest&mmask)^(m&mmask)).count();
 				bitcount[n] += mmask.count();
 			}
@@ -150,7 +158,7 @@ int main(int argc, char* argv[]){
 		cout<<"Run length: "<<std::get<4>(stat[target])<<endl;
 		cout<<"Noise factor"
 		<<"\tBER"
-		<<"\tDER"
+		<<"\tNER"
 		<<endl;
 		for(size_t n=0; n<noise_factor.size(); n++){
 			cout<<noise_factor[n]
