f67c54769f73b8f1d61342e0bc92b12f1e942bbb
HK_Koutan <kouseisinn@gmail.com>
2023-12-17 21:14:20 +0900

    feature: エラー処理を追加

--- src/common/memory.cuh
+++ src/common/memory.cuh
@@ -1,11 +1,13 @@
 #include <memory>
+#include <exception>
 
 namespace util{
 
 template<typename T>
 struct cuda_delete {
 	void operator()(void* p) const{
-		::cudaFree(p);
+		auto errc = ::cudaFree(p);
+		if(errc!=0) throw std::runtime_error("CUDA Error");
 	}
 };
 
@@ -13,14 +15,16 @@ template<typename T>
 typename std::enable_if<std::is_array<T>::value, std::unique_ptr<T,cuda_delete<T>>>::type make_cuda_unique(const std::size_t n){
 	using U = std::remove_extent<T>::type;
 	U* p = nullptr;
-	::cudaMalloc(reinterpret_cast<void**>(&p), sizeof(U)*n);
+	auto errc = ::cudaMalloc(reinterpret_cast<void**>(&p), sizeof(U)*n);
+	if(errc!=0) throw std::runtime_error("CUDA Error");
 	return std::unique_ptr<T,cuda_delete<T>>{p};
 }
 
 template<typename T>
 typename std::unique_ptr<T,cuda_delete<T>>::type make_cuda_unique(){
 	T* p = nullptr;
-	::cudaMalloc(reinterpret_cast<void**>(&p), sizeof(T));
+	auto errc = ::cudaMalloc(reinterpret_cast<void**>(&p), sizeof(T));
+	if(errc!=0) throw std::runtime_error("CUDA Error");
 	return std::unique_ptr<T,cuda_delete<T>>{p};
 }
 
