8272ed07b68630cb2abb54140ff8611d1d363f56
HK_Koutan <kouseisinn@gmail.com>
2023-12-19 19:43:42 +0900

    refactor: cuda_deleteをテンプレート解除

--- src/common/memory.cuh
+++ src/common/memory.cuh
@@ -2,7 +2,6 @@
 
 namespace util{
 
-template<typename T>
 struct cuda_delete {
 	void operator()(void* p) const{
 		::cudaFree(p);
@@ -10,18 +9,18 @@ struct cuda_delete {
 };
 
 template<typename T>
-typename std::enable_if<std::is_array<T>::value, std::unique_ptr<T,cuda_delete<T>>>::type make_cuda_unique(const std::size_t n){
+typename std::enable_if<std::is_array<T>::value, std::unique_ptr<T,cuda_delete>>::type make_cuda_unique(const std::size_t n){
 	using U = std::remove_extent<T>::type;
 	U* p = nullptr;
 	::cudaMalloc(reinterpret_cast<void**>(&p), sizeof(U)*n);
-	return std::unique_ptr<T,cuda_delete<T>>{p};
+	return std::unique_ptr<T,cuda_delete>{p};
 }
 
 template<typename T>
-typename std::unique_ptr<T,cuda_delete<T>>::type make_cuda_unique(){
+typename std::unique_ptr<T,cuda_delete>::type make_cuda_unique(){
 	T* p = nullptr;
 	::cudaMalloc(reinterpret_cast<void**>(&p), sizeof(T));
-	return std::unique_ptr<T,cuda_delete<T>>{p};
+	return std::unique_ptr<T,cuda_delete>{p};
 }
 
 }
