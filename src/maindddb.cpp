60b03b18e4efa4401f91a98e6b1858ad35426e13
HK_Koutan <kouseisinn@gmail.com>
2024-01-14 00:07:56 +0900

    test: 3.12.2 dddb 3 BS=32

--- src/maindddb.cpp
+++ src/maindddb.cpp
@@ -19,7 +19,7 @@ constexpr size_t DEFAULT_REPEAT_PER_THREAD = 5000;
 constexpr size_t SOURCE_LENGTH = 512;
 constexpr size_t CODE_LENGTH = 1024;
 constexpr size_t NUM_THREADS = 20;
-constexpr size_t BLOCK_SIZE = 16;
+constexpr size_t BLOCK_SIZE = 32;
 constexpr uint8_t ATGC = 0x1B;
 constexpr double TOLERANCE = 0.125;
 
